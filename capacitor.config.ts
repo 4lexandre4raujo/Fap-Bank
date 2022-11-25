454fd423b96cc5b61f856846fc2973b67a440e9f
Alexandre Araújo <alexandresaraujo19@gmail.com>
2022-11-25 01:27:46 -0300

    first comit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Digital-account-OCR',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
